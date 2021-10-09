21324021847b698bc007490c2cc5490c0de4f7d5
Weston Knowlton <kno18012@byui.edu>
2021-10-09 15:27:59 -0600

    implemented document pages, added dropdown directive

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(1, 'Test document 1', 'Document description 1', 'test@test.com', []),
+    new Document(1, 'Test document 2', 'Document description 2', 'test@test.com', []),
+    new Document(1, 'Test document 3', 'Document description 3', 'test@test.com', []),
+    new Document(1, 'Test document 4', 'Document description 4', 'test@test.com', []),
+    new Document(1, 'Test document 5', 'Document description 5', 'test@test.com', [])
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
